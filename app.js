345aacf302e7da82c678bbb95e0edfc67cdc0082
Lucineia Silva <90657609+LucineiaSilvah@users.noreply.github.com>
2024-02-09 21:39:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,25 +7,25 @@ const botao = document.querySelector("button");
 let resp = document.querySelector(".resp");
 
 botao.addEventListener("click", () => {
-  if (B.value == "X".toLowerCase()) {
+  if (B.value == "X") {
     B = 1;
     let x = parseInt(C.value) * B;
     let resposta = (parseInt(A.value) * parseInt(D.value)) / x;
     resp.innerHTML ='X = ' + resposta;
 
 
-  } else if (D.value == "X".toLowerCase()) {
+  } else if (D.value == "X") {
     D = 1;
     let x = A.value * D;
     let resposta = (parseInt(C.value) * parseInt(B.value)) / x;
     resp.innerHTML ='X = ' + resposta;
-  }else if (A.value == "X".toLowerCase()) {
+  }else if (A.value == "X") {
     A = 1;
     let x = D.value * A;
     let resposta = (parseInt(C.value) * parseInt(B.value)) / x;
     resp.innerHTML ='X = ' + resposta;
   }
-  else if (C.value == "X".toLowerCase()) {
+  else if (C.value == "X") {
     C = 1;
     let x = B.value * C;
     let resposta = (parseInt(A.value) * parseInt(D.value)) / x;
